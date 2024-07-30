5d2dcbedbbfef3c16ff604334a83ac9e366699e4
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2024-07-30 23:51:50 +0300

    add resume title to constants

--- astro/src/constants/index.ts
+++ astro/src/constants/index.ts
@@ -3,3 +3,9 @@ export const POSITIONS = {
   BACKEND: "backend",
   FULLSTACK: "fullstack",
 };
+
+export const RESUME_TITLE = {
+  [POSITIONS.FRONTEND]: "Ahmed-Amer.pdf",
+  [POSITIONS.BACKEND]: "ahmed_amer.pdf",
+  [POSITIONS.FULLSTACK]: "Ahmed-Mohammed-Amer.pdf",
+};
